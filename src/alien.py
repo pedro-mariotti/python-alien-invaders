0d8745c84d44d7186f0f108d7f6571adcf0477aa
PEDRO MARIOTTI DE MOURA <162.630948@universidadesenaicimatec.edu.br>
2026-08-20 10:14:19 -0300

    feature: add req.txt and monkeytype typing

--- src/alien.py
+++ src/alien.py
@@ -11,7 +11,7 @@ class Alien(Sprite):
         self.settings = alien_invasion_settings
         
         # Carrega a imagem do alienígena e obtém seu rect
-        self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.image.load('../images/alien.bmp')
         self.rect = self.image.get_rect()
         
         # Inicia cada novo alienígena próximo à parte superior esquerda da tela
